f2ecde69b17ba2b7b0dc83f825b7a7275ef18fed
tatKOM <vlasovich.2019@gmail.com>
2023-04-11 19:22:46 +0700

    -

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
-#open new horizons
-cp -rf sources.list /etc/apt/
-apt update
- 
+
 # installing packages from 'packages' file
 apt-get install $(grep -vE "^\s*#" packages  | tr "\n" " ")
 
@@ -17,14 +14,6 @@ make install installsystemd
 systemctl enable ly.service
 cd ../
 rm -rf ly
-
-# add kali-linux repository's and other(open new horizons 2)
-echo ""
-wget 'https://archive.kali.org/archive-key.asc'
-apt-key add archive-key.asc
-apt update
-apt install betterlockscreen
-cp -rf sources.list.d ../../../etc/apt/
 
 #config install
 cp -rf Baza/.config/* ~/.config/
